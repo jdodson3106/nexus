6c211b42769f7a66beab0720b6b2008ed6857fa1
Justin Dodson <justin.dodson@agoradata.com>
2024-09-26 13:47:49 -0500

    clean up master

--- nexus.go
+++ nexus.go
@@ -1,13 +1,13 @@
 package nexus
 
 import (
+	"context"
 	"fmt"
+	"github.com/a-h/templ"
+	"github.com/jdodson3106/nexus/internal/db"
+	"github.com/jdodson3106/nexus/log"
 	"net/http"
 	"os"
-
-	"github.com/jdodson3106/nexus/internal/db"
-	log "github.com/jdodson3106/nexus/log"
-	"github.com/julienschmidt/httprouter"
 )
 
 var viewsPath string
@@ -23,20 +23,71 @@ const (
 	DEF_APP_NAME = "app"
 )
 
-// HandlerFunc the stub of a handler method that should be passed in any Nexus route
-type HandlerFunc func(ctx *Context) error
-
 type RenderArgs struct {
 	Args       map[string]interface{}
 	RenderFunc string
 }
 
-func (n *Nexus) CloseDatabase() error {
-	return n.db.Close()
+type Nexus struct {
+	port    string
+	router  *Router
+	appName string
+	config  NexusConfig
+
+	// TODO: implement db abstraction
+	db *db.DB
 }
 
-// the default setup
-func NewDefault() (*Nexus, error) {
+type Context struct {
+	Request  *http.Request
+	Response http.ResponseWriter
+	Context  context.Context
+	Params   Params
+	context  context.Context
+}
+
+func (ctx *Context) Render(name string, args *RenderArgs) error {
+	path := fmt.Sprintf("%s/%s_templ.go", viewsPath, name)
+	comp, err := reflectiveRender(name, path, args)
+	if err != nil {
+		return err
+	}
+	return comp.Render(ctx.context, ctx.Response)
+}
+
+func (ctx *Context) RenderComponent(component templ.Component) error {
+	return component.Render(ctx.context, ctx.Response)
+}
+
+func NewContext(w http.ResponseWriter, r *http.Request, params Params) *Context {
+	return &Context{
+		Request:  r,
+		Response: w,
+		Params:   params,
+		context:  context.Background(),
+	}
+}
+
+// NexusConfig: Defines all custom config in here
+// this will be generated during the scaffolding proces
+// if the user provides flags to the cli tool
+type NexusConfig struct {
+	Port          string
+	ViewPath      string
+	controllerDir string
+}
+
+type controller struct {
+	path   string
+	name   string
+	routes []Route
+}
+
+type controllerRegister struct {
+	controllers []controller
+}
+
+func InitNexus() (*Nexus, error) {
 	dir, err := os.Getwd()
 	if err != nil {
 		dir = fmt.Sprintf("./%s", DEF_APP_NAME)
@@ -46,26 +97,31 @@ func NewDefault() (*Nexus, error) {
 	dir += "/views"
 	viewsPath = dir
 
-	db, err := db.NewDefaultDbConnection()
+	// create the new nexus instance
+	n := &Nexus{port: ":8080", appName: DEF_APP_NAME}
+
+	// register the existing routes and controller methods (handlers)
+	n.router = NewRouter("/")
+
+	// TODO: Call all found router methods
+	n.generateRoutes(n.router)
+
+	// parse the controller files
+	err = n.parseControllers()
 	if err != nil {
+		log.Error(err.Error())
 		return nil, err
 	}
 
-	return &Nexus{
-		router:  NewRouter(""),
-		port:    ":8080",
-		appName: DEF_APP_NAME,
-		db:      db,
-	}, nil
+	return n, nil
 }
 
-// the custom setup given cli flags
-func New(c NexusConfig) (*Nexus, error) {
-	// WIP
-	viewsPath = c.ViewPath
+func InitNexusWithConfig(config NexusConfig) (*Nexus, error) {
+	viewsPath = config.ViewPath
 	return &Nexus{
-		router: NewRouter(""),
-		port:   c.Port,
+		appName: DEF_APP_NAME,
+		router:  NewRouter(""),
+		port:    config.Port,
 	}, nil
 }
 
@@ -91,39 +147,18 @@ func (n *Nexus) Run() error {
 	return http.ListenAndServe(n.port, n.router.httpRouter)
 }
 
-func (n *Nexus) GET(path string, handler HandlerFunc) {
-	n.createHttpHandle(GET, path, handler)
+func (n *Nexus) CloseDatabase() error {
+	return n.db.Close()
 }
 
-func (n *Nexus) POST(path string, handler HandlerFunc) {
-	n.createHttpHandle(POST, path, handler)
+func (n *Nexus) parseControllers() error {
+	// TODO: search through the application's path and find the controller dir
+	// and hash the files and store the hashes in a application data file
+	// diff the hashes of each file to determine if the file needs to be reparsed
+
+	return nil
 }
 
-func (n *Nexus) PUT(path string, handler HandlerFunc) {
-	n.createHttpHandle(PUT, path, handler)
-}
+func (n *Nexus) generateRoutes(router *Router) {
 
-func (n *Nexus) PATCH(path string, handler HandlerFunc) {
-	n.createHttpHandle(PATCH, path, handler)
-}
-
-func (n *Nexus) DELETE(path string, handler HandlerFunc) {
-	n.createHttpHandle(DELETE, path, handler)
-}
-
-// TODO: Git rid of all this and use updates in the new Router
-func (n *Nexus) createHttpHandle(method string, path string, handler HandlerFunc) {
-	handle := func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		var newParams Params
-		for _, oldParam := range p {
-			newParams = append(newParams, Param{Key: oldParam.Key, Value: oldParam.Value})
-		}
-		ctx := NewContext(w, r, newParams)
-		if err := handler(ctx); err != nil {
-			// todo: figure out handling errors in the handler calls;
-			//       need a default mechanism to handle these gracefully
-			panic(err)
-		}
-	}
-	n.router.Handle(method, path, handle)
 }
